958663f47ee06b91d0741cb82a77902359de858c
Hary051 <145652213+Hary051@users.noreply.github.com>
2023-10-12 09:42:55 +0200

    Add files via upload

---
+++ kalkulačka/kalkulačka/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int pocitac1;
+int pocitac2;
+bool success;
+bool success1;
+do
+{
+
+
+Console.WriteLine("Číslo 1");
+string cislo1 = Console.ReadLine();
+success = int.TryParse(cislo1, out pocitac1);
+
+
+
+Console.WriteLine("Číslo 2");
+string cislo2 = Console.ReadLine();
+success1 = int.TryParse(cislo2, out pocitac2);
+    if (success1==false || success==false)
+    {
+        Console.WriteLine("špatné číslo");
+    }
+}
+while (!success || !success1);
+
+Console.WriteLine("Zvol Operaci");
+string operace = Console.ReadLine();
+
+
+do
+{
+
+
+if (operace == "+")
+{
+  Console.WriteLine(pocitac1+pocitac2);
+}
+else if (operace == "-")
+{
+    Console.WriteLine(pocitac1 - pocitac2);
+}
+else if (operace == "*")
+{
+    Console.WriteLine(pocitac1 * pocitac2);
+}
+else if (operace == "/")
+{
+    Console.WriteLine(pocitac1 / pocitac2);
+}
+else
+{
+    Console.WriteLine("špatné číslo");
+}
+}
+while 
