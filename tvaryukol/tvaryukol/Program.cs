1f3387863c1d737d62fdcf3efb471a846938f968
Hary051 <145652213+Hary051@users.noreply.github.com>
2023-11-23 11:48:40 +0100

    Add files via upload

---
+++ tvaryukol/tvaryukol/Program.cs
@@ -0,0 +1,17 @@
+﻿
+string[] tvary = new string[] { "trojúhelník ", "čtverec ", "kruh ", "obdelník ", "pětiúhelník ", "krychle ", "kvádr ", "koule" };
+
+
+int vyber()
+{
+    
+    for (int i = 0; i < tvary.Length; i++)
+    {
+        Console.WriteLine(i + " = "+ tvary[i]);
+    }
+    string kterej = Console.ReadLine();
+    int.TryParse(kterej, out int cislo1);
+    return cislo1;
+}
+int a=vyber();
+Console.WriteLine(a);
